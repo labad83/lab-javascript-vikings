f344edbcb730c2b0beba1326994325063c3ceb61
laura <lauraabad83@gmail.com>
2019-03-24 17:15:30 +0100

    added soldier and half viking

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,35 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage(damage) {
+        return this.health -= damage;
+    }
+
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+
+    }
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage(damage) {
+        return Soldier.receiveDamage;
+    }
+}
 
 // Saxon
 class Saxon {}
